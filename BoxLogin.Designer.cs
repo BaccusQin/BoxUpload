f298b4dd6598c54e96f5fdb3b139d4a1a3706a13
haoyu qin <lxliuyuan@hotmail.com>
2020-04-21 17:30:40 +0900

    プロジェクト ファイルを追加します。

---
+++ BoxLogin.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace BoxUpload
+{
+    partial class BoxLogin
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.ShowUser = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbUserName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(214, -1);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(672, 480);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // ShowUser
+            // 
+            this.ShowUser.Location = new System.Drawing.Point(49, 56);
+            this.ShowUser.Name = "ShowUser";
+            this.ShowUser.Size = new System.Drawing.Size(78, 23);
+            this.ShowUser.TabIndex = 1;
+            this.ShowUser.Text = "データ連携";
+            this.ShowUser.UseVisualStyleBackColor = true;
+            this.ShowUser.Click += new System.EventHandler(this.ShowUser_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ユーザ名:";
+   
+            // 
+            // lbUserName
+            // 
+            this.lbUserName.AutoSize = true;
+            this.lbUserName.Location = new System.Drawing.Point(73, 18);
+            this.lbUserName.Name = "lbUserName";
+            this.lbUserName.Size = new System.Drawing.Size(35, 12);
+            this.lbUserName.TabIndex = 3;
+            this.lbUserName.Text = "label2";
+            // 
+            // BoxLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 478);
+            this.Controls.Add(this.lbUserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ShowUser);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "BoxLogin";
+            this.Text = "BoxLogin";
+            this.Load += new System.EventHandler(this.BoxLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button ShowUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbUserName;
+    }
+}
+
---
+++ BoxLogin.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json.Linq;
+using Box.V2.Config;
+using Box.V2.Auth;
+using Box.V2;
+using Box.V2.Models;
+
+namespace BoxUpload
+{
+    public partial class BoxLogin : Form
+    {
+        public string connCode { get; set; }
+        public static string token { get; set; }
+
+        public static string sBoxClientId { get; set; }
+
+        public static string sBoxClientSecret { get; set; }
+
+        
+        public BoxLogin()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BoxLogin_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                sBoxClientId = "7lbnr90zubwiz6ddjdvjko3h8ytk67f8";
+                sBoxClientSecret = "iI4AfSkyHVdFJQYbei2gVUXGg3iEUSaX";
+                string strHtml = LoginPage_Load();
+            
+                webBrowser1.ScriptErrorsSuppressed= true;
+                webBrowser1.Navigate("about:blank");
+                webBrowser1.Document.OpenNew(true);
+                webBrowser1.Document.Write(strHtml);
+
+        
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("エラー："+ex.Message);
+                Application.Exit();
+            }
+          
+
+        }
+        private string LoginPage_Load( )
+        {
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)(0xc0 | 0x300 | 0xc00);
+        
+            string sUrl = "https://account.box.com/api/oauth2/authorize";
+            string sPostData = "response_type=code&client_id=" +
+            HttpUtility.UrlEncode(sBoxClientId);
+
+            try
+            {
+                System.Text.Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
+                byte[] postDataBytes = System.Text.Encoding.ASCII.GetBytes(sPostData);
+                System.Net.WebRequest req = System.Net.WebRequest.Create(sUrl);
+                req.Method = "POST";
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.ContentLength = postDataBytes.Length;
+                System.IO.Stream reqStream = req.GetRequestStream();
+                reqStream.Write(postDataBytes, 0, postDataBytes.Length);
+                reqStream.Close();
+                System.Net.WebResponse res = req.GetResponse();
+                System.IO.Stream resStream = res.GetResponseStream();
+                System.IO.StreamReader sr = new System.IO.StreamReader(resStream, enc);
+                string js = sr.ReadToEnd();
+                sr.Close();
+                return js;
+
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+
+        }
+
+        private void ShowUser_Click(object sender, EventArgs e)
+        {
+            
+            if (webBrowser1.Url != null)
+            {
+                webBrowser1.Visible = false;
+                MessageBox.Show(webBrowser1.Url.ToString());
+                connCode = webBrowser1.Url.ToString();
+                string code =connCode.Split('?')[1].Substring(5);
+                BoxConn boxConn = new BoxConn(code,sBoxClientId,sBoxClientSecret);
+                token=boxConn.token;
+                SetForm.GetInfo();
+                lbUserName.Text = SetForm.UserName;
+            }
+            else
+            {
+                MessageBox.Show("Boxにログインしてください。");
+            }
+            
+        }
+
+       
+    }
+    public class SetForm
+    {
+        public static string UserName { get; set; }
+        public static async Task GetInfo()
+        {
+            var config = new BoxConfig(BoxLogin.sBoxClientId, BoxLogin.sBoxClientSecret, new Uri("http://localhost"));
+            var session = new OAuthSession(BoxLogin.token, "NOT_NEEDED", 3600, "bearer");
+            var client = new BoxClient(config, session);
+            BoxUser currentUser = await client.UsersManager.GetCurrentUserInformationAsync();
+            UserName = currentUser.Name;
+            
+        }
+       
+
+    }
+    public class BoxConn
+    {
+        public string token { get; set; }
+        public BoxConn(string code,string sBoxClientId,string sBoxClientSecret)
+        {
+      
+            string sUrl = "https://api.box.com/oauth2/token";
+            string accessToken;
+            try
+            {
+
+                System.Net.HttpWebRequest req = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(sUrl);
+                System.Text.Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
+                req.Method = "POST";
+                req.ContentType = "application/x-www-form-urlencoded";
+                string strRequest = "client_id=" + sBoxClientId + "&" + "client_secret=" + sBoxClientSecret + "&code=" + code + "&grant_type=authorization_code";
+                byte[] pd = new UTF8Encoding().GetBytes(strRequest);
+                req.ContentLength = pd.Length;
+                Stream ps = req.GetRequestStream();
+                ps.Write(pd, 0, pd.Length);
+                ps.Close();
+                System.Net.WebResponse res = req.GetResponse();
+                System.IO.Stream resStream = res.GetResponseStream();
+                System.IO.StreamReader sr = new System.IO.StreamReader(resStream, enc);
+                string js = sr.ReadToEnd();
+                JObject Json = JObject.Parse(js);
+                accessToken = Json["access_token"].ToString();
+                sr.Close();
+                token= accessToken;
+            
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+              
+            }
+
+        }
+
+    }
+}
